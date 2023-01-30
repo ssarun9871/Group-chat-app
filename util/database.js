741d3a32b86571d9e4a864dd8031665c8324b679
ssarun9871 <ssarun9871@gmail.com>
2023-01-30 16:20:25 +0530

    ready for deploy

--- util/database.js
+++ util/database.js
@@ -1,11 +1,22 @@
+// const Sequelize = require('sequelize');
+
+// const sequelize = new Sequelize("group_chat", "root","1234",{
+//     dialect:"mysql",
+//     host:"localhost",
+//     define: { 
+//         timestamps: false
+//       }
+// })
+// module.exports = sequelize; 
+
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize("group_chat", "root","1234",{
+const sequelize = new Sequelize(process.env.DB_database, process.env.DB_user,process.env.DB_password,{
     dialect:"mysql",
-    host:"localhost",
+    host:process.env.DB_host,
     define: { 
         timestamps: false
       }
 })
-module.exports = sequelize; 
+module.exports = sequelize;
